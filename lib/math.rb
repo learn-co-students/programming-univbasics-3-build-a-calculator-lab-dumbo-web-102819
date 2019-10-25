6236ad0ed11233b7cc92f6ae48a600ac8fe16079
exuberant-pool-9484 <alisherfayzimatov@gmail.com>
2019-10-25 17:04:34 +0000

    Done.

--- lib/math.rb
+++ lib/math.rb
@@ -1,23 +1,23 @@
 def addition(num1, num2)
-puts num1+num2
+ num1+num2
 end
 
 def subtraction(num1, num2)
-puts num1-num2
+ num1-num2
 end
 
 def division(num1, num2)
-puts num1/num2
+ num1/num2
 end
 
 def multiplication(num1, num2)
-puts num1*num2
+ num1*num2
 end
 
 def modulo(num1, num2)
-puts num2.Math.float /num1.Math.float
+ num1 % num2
 end
 
 def square_root(num)
-puts num.Math.sqrt
+ Math.sqrt(num)
 end
